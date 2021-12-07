85c80dd5156b9ebf99cfb2125bdc83859e04a0f8
Geovani Ferreira <gugaheadbanger@gmail.com>
2021-12-07 11:59:49 -0300

    Consertando Repository

--- LojaDeGames/src/main/java/org/generation/LojaDeGames/repository/CategoriaRepository.java
+++ LojaDeGames/src/main/java/org/generation/LojaDeGames/repository/CategoriaRepository.java
@@ -3,8 +3,9 @@ package org.generation.LojaDeGames.repository;
 import java.util.List;
 
 import org.generation.LojaDeGames.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriaRepository extends JPARepository<Categoria, Long> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 	public List<Categoria> findAllByDescricaoContainingIgnoreCase (String descricao);
 	
